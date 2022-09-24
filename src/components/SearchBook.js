a538a4dc6a609b6b83e772fae5a716a67cf8d0e6
MohamedDiaaEldin <mdiaan404@gmail.com>
2022-09-24 20:10:37 +0200

    implement get main page data and rendering it

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -7,7 +7,6 @@ const SearchBook = ()=>{
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to='/'>Close</Link>
-
           <div className="search-books-input-wrapper">
             <input type="text" placeholder="Search by title, author, or ISBN" />
           </div>
